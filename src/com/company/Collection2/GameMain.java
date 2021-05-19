33af8e4747aae167dd6bbcaea2ea79ca70b12d12
Ran You <ran.you8311@gmail.com>
2021-05-19 12:48:02 -0700

    Array exercise

--- src/com/company/Collection2/GameMain.java
+++ src/com/company/Collection2/GameMain.java
@@ -16,17 +16,61 @@ public class GameMain {
         locations.put(4, new Location(4,"E"));
         locations.put(5, new Location(5,"F"));
 
+        locations.get(1).addExit("W",2);
+        locations.get(1).addExit("E",3);
+        locations.get(1).addExit("S",4);
+        locations.get(1).addExit("N",5);
+
+
+        locations.get(2).addExit("N",5);
+
+        locations.get(3).addExit("W",1);
+
+
+        locations.get(4).addExit("N",1);
+        locations.get(4).addExit("W",2);
+
+
+        locations.get(5).addExit("S",1);
+        locations.get(5).addExit("W",2);
+
+        Map<String,String> vocabulory= new HashMap<String,String >();
+        vocabulory.put("NORTH","N");
+        vocabulory.put("SOUTH","S");
+        vocabulory.put("EAST","E");
+        vocabulory.put("WEST","W");
+        vocabulory.put("QUIT","Q");
+
+
         int loc=1;
         while (true){
-            System.out.println(locations.get(loc).getDescription());
+            System.out.println(" You are currently at location "+locations.get(loc).getDescription());
             if (loc ==0){
                 break;
             }
+            Map <String,Integer>exits=locations.get(loc).getExits();
+            System.out.print("Available exits are ");
+            for (String exit: exits.keySet()){
+                System.out.print(exit+ ", ");
+            }
+            System.out.println();
 
-        loc= scanner.nextInt();
-        scanner.nextLine();
-        if (!locations.containsKey(loc)){
-            System.out.println("You can not go that direction");
+        String direction= scanner.nextLine().toUpperCase();
+
+        if (direction.length()>1){
+            String[] words=direction.split(" ");
+            for (String word: words){
+                if(vocabulory.containsKey(word)){
+                    direction=vocabulory.get(word);
+                }
+            }
+        }
+            System.out.println("your input is " + direction);
+
+        if (exits.containsKey(direction)){
+            loc=exits.get(direction);
+        } else {
+            System.out.println("You cannot go in that direction");
         }
     }
 
